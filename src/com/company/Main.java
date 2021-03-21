83e62f0ded003224da71d122b10da29605065085
aimless-cpu <s.grabher@protonmail.com>
2021-03-21 17:56:11 +0100

    hope

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {10,9,10,10,10,10,10,10,10,10};
+        int v=0;
+
+
+        for (int i=0;i<intArray.length;i++) {
+            //System.out.println(i);
+
+            v = v+intArray[i];
+            System.out.println(v);
+
+        }
+
+
+    }
+}
